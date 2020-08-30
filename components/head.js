044e95bac718a302cac7f36b927b30118d768f88
Harris <harrisconnected@gmail.com>
2020-08-30 13:20:08 +0530

    Fix head-count error due to html lang -_-

--- components/head.js
+++ components/head.js
@@ -5,13 +5,17 @@ export default ({ children }) => (
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-    <html lang="en-US"></html>
-    {children}
 
+    <script
+      async
+      defer
+      data-website-id="233c882a-636a-4913-bbbc-520b60bc00db"
+      src="https://umami.harrisjose.dev/umami.js"
+    ></script>
     <script
       dangerouslySetInnerHTML={{
         __html: `
-          var pref1 = localStorage.getItem('hpj-theme');
+          var pref1 = localStorage.getItem('hpj-theme')
           var pref2 = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
           if (pref1) {
             document.documentElement.setAttribute('data-theme', pref1)
@@ -21,12 +25,6 @@ export default ({ children }) => (
         `,
       }}
     ></script>
-
-    <script
-      async
-      defer
-      data-website-id="233c882a-636a-4913-bbbc-520b60bc00db"
-      src="https://umami.harrisjose.dev/umami.js"
-    ></script>
+    {children}
   </Head>
 )
