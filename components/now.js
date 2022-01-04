c4e285bd60b8f1e137ac72851717bebd19922e67
harris <harrisjose@outlook.com>
2022-01-04 12:01:05 +0530

    Add now page

--- components/now.js
+++ components/now.js
@@ -1,12 +1,22 @@
 import { useNowPlaying } from 'hooks/use-spotify'
 import SpotifyIcon from '@/icons/music.svg'
+import ActivityIcon from '@/icons/activity.svg'
 
 const Now = ({}) => {
   const song = useNowPlaying()
 
   return (
     <section className="mt-16">
-      <h2 className="font-semibold">Now</h2>
+      <h2 className="font-semibold flex items-center">
+        Now{' '}
+        <a
+          href="https://nownownow.com/about"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ActivityIcon className="inline w-4 h-4 text-dim hover:text-light cursor-pointer mx-2" />
+        </a>
+      </h2>
       <div className="text-light mt-6">
         Whipping up a text editor and thinking about presentations on the web.
         Getting my hands dirty with Node.js and Rust.
@@ -20,7 +30,7 @@ const Now = ({}) => {
           className="mt-6 text-dim flex items-center group"
         >
           <SpotifyIcon className="h-5 w-5 mr-2.5 text-green-400"></SpotifyIcon>
-          <div>
+          <div className="text-sm">
             <span className="text-light group-hover:text-dark">
               {song?.name}
             </span>{' '}
---
+++ pages/now.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Link from 'next/link'
+import Page from '@/components/page'
+import Head from '@/components/head'
+import Contact from '@/components/contact'
+import Now from '@/components/now'
+import Bio from '@/components/bio'
+
+const Home = ({ posts }) => {
+  return (
+    <Page>
+      <Head></Head>
+
+      <main className="container max-w-screen-sm mx-auto mb-16">
+        <Bio></Bio>
+        <Now />
+
+        <section className="mt-16">
+          <Link href={'/'}>
+            <div className="text-dim underline italic cursor-pointer hover:text-light">
+              go home →
+            </div>
+          </Link>
+        </section>
+      </main>
+    </Page>
+  )
+}
+
+export default Home
